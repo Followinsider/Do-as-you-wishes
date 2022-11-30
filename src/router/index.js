83f143b090661ab849cd0bb8d4f042950214453f
L_CX <78329926+Followinsider@users.noreply.github.com>
2022-11-30 11:04:18 +0800

    Update routes mode from "history" to "hash"

--- src/router/index.js
+++ src/router/index.js
@@ -156,8 +156,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+//   mode: "history",
+//   base: process.env.BASE_URL,
   routes,
 });
 
